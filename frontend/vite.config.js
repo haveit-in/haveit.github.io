392c2f16101147d888dfe103aea0a33ec97c56a1
ajaygugul8 <welcomeajaykumar007@gmail.com>
2026-03-11 14:49:56 +0530

    changes done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     fs: {
-//       // Allow importing assets from repo root (e.g. ../image/*)
-//       allow: ['..'],
-//     },
-//   },
-// })
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/haveit.github.io/",
 })
